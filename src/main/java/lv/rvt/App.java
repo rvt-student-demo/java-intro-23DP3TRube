00a17d9d51e7f4001004b500ccd763043380894a
23DP3TRube <166013447+23DP3TRube@users.noreply.github.com>
2024-09-06 08:15:44 +0000

    Uzdevums 0

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,11 +1,23 @@
 package lv.rvt;
-
+import java.util.Scanner;
 public class App 
 {
     public static void main( String[] args )
     {
-        // Make your first Hello World app!
 
+    
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("ievadi vardu:");
+        String name = scanner.nextLine();
+        
+        System.out.println("ievadi uzvardu:");
+        String uzvards = scanner.nextLine();
+
+        System.out.println("ievadi grupu:");
+        String grupa = scanner.nextLine();
+
+        System.out.println("Students: "+ name + " " + uzvards + ", " + "grupa: " + grupa);
+        
     }
-
 }
