f33c433cfb0f9d3a9c0854feb58e8349bd22a903
23DP3TRube <166013447+23DP3TRube@users.noreply.github.com>
2024-09-20 07:57:51 +0000

    Grades and Points

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,9 +8,25 @@ public class App
     
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Ievadi punktus [0-100]: ");
         int number = Integer.valueOf(scanner.nextLine());
-        if (number > 135) {
-            System.out.println("Speeding ticket!");
-           } else {System.out.println("Speeding ticket fakeo!");}
+
+        if (number < 0) {
+            System.out.println("Neiespējams");
+        } else if (number < 50) {
+            System.out.println("Neizdevas");
+        } else if (number < 60) {
+            System.out.println("Grade: 1");
+        } else if (number < 70) {
+            System.out.println("Grade: 2");
+        }else if (number < 80) {
+            System.out.println("Grade: 3");
+        }else if (number < 90) {
+            System.out.println("Grade: 4");
+        }else if (number < 100) {
+            System.out.println("Grade: 5");
+        }else {
+            System.out.println("Izcili");
+        }
     }
 }
