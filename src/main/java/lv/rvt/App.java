717fef43dd6d556d5f6ef07eee5069c3894a15f6
23DP3TRube <166013447+23DP3TRube@users.noreply.github.com>
2024-09-27 08:09:20 +0000

    Number of Numbers

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,19 +8,22 @@ public class App
     
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Value of the gift?");
-        int gift = Integer.valueOf(scanner.nextLine());
-    
-        if (gift >= 5000 && gift <= 25000) {
-            System.out.println("Tax: " + (100 +(gift - 5000) * 0.08) );
-        } else if (gift >= 25000 && gift <= 55000) {
-            System.out.println("Tax: " + (1700 +(gift - 25000) * 0.1) );
-        } else if (gift >= 55000 && gift <= 200000) {
-            System.out.println("Tax: " + (4700 +(gift - 55000) * 0.12) );
-        } else if (gift >= 200000 && gift <= 1000000) {
-            System.out.println("Tax: " + (22100 +(gift - 200000) * 0.15) );
-        } else if (gift >= 1000000 && gift <= 10000000) {
-            System.out.println("Tax: " + (142100 +(gift - 1000000) * 0.17) );
-        } else {System.out.println("No tax!");}
+        int nums = 0;
+        int sum = 0;
+
+        while (true) {
+            System.out.println("Give the number 0 to end: ");
+
+            int number = Integer.valueOf(scanner.nextLine());
+        
+            if (number != 0) {
+                sum = sum + number;
+                nums =  nums + 1;
+                continue;
+            }
+            break;
+        }
+        System.out.println("The ammount of number: " + nums);
+        System.out.println("The total sum: " + sum);
 }
 }
