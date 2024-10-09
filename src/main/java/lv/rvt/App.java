cc6302b8e47f8659f02d7e37539c372b19a4352f
23DP3TRube <166013447+23DP3TRube@users.noreply.github.com>
2024-10-09 07:02:30 +0000

    Divisible by three (cilveka ievadi)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,14 +1,25 @@
 package lv.rvt;
+
 import java.util.Scanner;
+
 public class App 
 {
     public static void main(String[] args) {
-        divisibleByThreeInRange(2, 10);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ievadi sakumu ");
+        int beginning = Integer.valueOf(scanner.nextLine());
+        System.out.print("Ievadi beigas ");
+        int end = Integer.valueOf(scanner.nextLine());
+
+        divisibleByThreeInRange(beginning, end);
+        
+        scanner.close();
     }
-    
-    public static void divisibleByThreeInRange(int beginning, int end){
-        while (beginning<=end) {
-            if (beginning % 3 == 0 ){
+
+    public static void divisibleByThreeInRange(int beginning, int end) {
+        while (beginning <= end) {
+            if (beginning % 3 == 0) {
                 System.out.println(beginning);
             }
             beginning++;
