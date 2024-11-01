e6db008461decfd678a91b0f6fc316e20e5a9573
23DP3TRube <166013447+23DP3TRube@users.noreply.github.com>
2024-11-01 09:11:01 +0000

    Personal Details(salabots un skaistaks)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,62 +1,38 @@
 package lv.rvt;
-import java.util.Random;
-import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
-            int burtuskaits = 0; 
-            String garakais = new String();
-            int gadi = 0;
-            int sakits = 0;
+        int burtuskaits = 0; 
+        String garakais = "";
+        int gadi = 0;
+        int sakits = 0;
 
-while (true) {
-    
-    String ievade = reader.nextLine();
-    if (reader.equals("")) {
-        break;
+        while (true) {
+            String ievade = reader.nextLine();
+            if (ievade.isEmpty()) {
+                break;
+            }
+
+            String[] dalas = ievade.split(",");
+            gadi += Integer.parseInt(dalas[1]);
+            sakits++;
+
+            int lengthOfTheWord = dalas[0].length();
+
+            if (lengthOfTheWord > burtuskaits) {
+                burtuskaits = lengthOfTheWord;
+                garakais = dalas[0];
+            }
+        }
+
+        // Calculate the average after the loop ends
+        if (sakits > 0) {
+            double videjais = (double)gadi / sakits;
+            System.out.println("Garakais vards: " + garakais);
+            System.out.println("Videjais dzimšanas gads: " + videjais);
+        }
     }
-
-    String[] dalas = ievade.split(",");
-    gadi = gadi + Integer.valueOf(dalas[1]);
-    sakits = sakits + 1;
-
-
-    int lengthOfTheWord = dalas[0].length();
-
-    if( burtuskaits <= lengthOfTheWord){
-
-        burtuskaits = lengthOfTheWord;
-
 }
-    if(dalas[0].length() == burtuskaits){
-
-        garakais = dalas[0];
-    
-    }
-    
-    gadi = gadi + Integer.valueOf(dalas[1]);
-    
-    sakits = sakits + 1;
-    
-    if(sakits > 0){
-    
-    double videjais = (1.0*gadi/sakits);
-
-
-    
-    
-    
-    System.out.println("Garakais vards: " + garakais);
-    
-    System.out.println("Videjais dzimšanas gads: " + videjais);
-    
-    }
-
-}}}
-
-    
