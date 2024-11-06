ee8f3daf719f82ca042b6ec01a102186fade5704
23DP3TRube <166013447+23DP3TRube@users.noreply.github.com>
2024-11-06 07:42:53 +0000

    Your first bank transfer

--- src/main/java/lv/rvt/Account.java
+++ src/main/java/lv/rvt/Account.java
@@ -1,20 +1,23 @@
 package lv.rvt;
 
 
-public class Account {
-    Account artosAccount = new Account("Arto's account", 100.00);
-    Account artosSwissAccount = new Account("Arto's account in Switzerland", 1000000.00);
+class Account{
+    
+    private double balance; 
+    private String owner;
 
-    System.out.println("Initial state");
-    System.out.println(artosAccount);
-    System.out.println(artosSwissAccount);
-
-    artosAccount.withdraw(20);
-    System.out.println("The balance of Arto's account is now: " + artosAccount.balance());
-    artosSwissAccount.deposit(200);
-    System.out.println("The balance of Arto's other account is now: " + artosSwissAccount.balance());
-
-    System.out.println("End state");
-    System.out.println(artosAccount);
-    System.out.println(artosSwissAccount);
+    public Account (String owner, double balance) {
+    this.balance = balance;
+    this.owner = owner;
+}
+    public void deposit (double amount) { this.balance = this.balance + amount;
+}
+    public void withdraw (double amount) { this.balance = this.balance - amount;
+}
+    public double balance() {
+        return this.balance;
+}
+@Override
+public String toString() { return this.owner + "balance:" + this.balance;
+}
 }
