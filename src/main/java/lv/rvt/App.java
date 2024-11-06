7c734807e590f3cfbc72e698f4b9387a276493da
23DP3TRube <166013447+23DP3TRube@users.noreply.github.com>
2024-11-06 08:44:44 +0000

    Person klase pabeigta

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,22 +5,12 @@ public class App {
     public static void main(String[] args) {
         
     
-
+            Person ada = new Person("Ada");
+            Person antti = new Person("Antti");
+            Person martin = new Person("Martin");
     
-Account Matthewsaccount = new Account("Matthews account ", 1000.00);
-Account Myaccount = new Account("My account ", 0.00);
-
-System.out.println("Initial state");
-System.out.println(Matthewsaccount);
-System.out.println(Myaccount);
-
-Matthewsaccount.withdraw(100);
-System.out.println("The balance of Matthews account is now: " + Matthewsaccount.balance());
-Myaccount.deposit(100);
-System.out.println("The balance of My account is now: " + Myaccount.balance());
-
-System.out.println("End state");
-System.out.println(Matthewsaccount);
-System.out.println(Myaccount);
+            ada.printPerson();
+            antti.printPerson();
+            martin.printPerson();
 
     }}
