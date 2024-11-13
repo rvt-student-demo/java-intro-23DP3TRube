9589c50f968abf74c35ac28c85e554334f91b444
23DP3TRube <166013447+23DP3TRube@users.noreply.github.com>
2024-11-13 12:45:08 +0000

    Statistics (4 parts)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,49 +1,72 @@
 package lv.rvt;
+
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        
-        DecreasingCounter part1 = new DecreasingCounter(10);
-
-        part1.printValue();
-
-        part1.decrement();
-        part1.printValue();
-
-        part1.decrement();
-        part1.printValue();
-
-        System.out.println();
-    
-
-        DecreasingCounter part2 = new DecreasingCounter(2);
-
-        part2.printValue();
-
-        part2.decrement();
-        part2.printValue();
-
-        part2.decrement();
-        part2.printValue();
-
-        part2.decrement();
-        part2.printValue();
-
+        System.out.println("Part 1");
+        Statistics statistics = new Statistics();
+        statistics.addNumber(3);
+        statistics.addNumber(5);
+        statistics.addNumber(1);
+        statistics.addNumber(2);
+        System.out.println("Count: " + statistics.getCount());
         System.out.println();
 
-        DecreasingCounter part3 = new DecreasingCounter(100);
-
-        part3.printValue();
-
-        part3.reset();
-        part3.printValue();
-
-        part3.decrement();
-        part3.printValue();
-
+// part 2
+        System.out.println("Part 2");
+        System.out.println("Sum: " + statistics.sum());
+        System.out.println("Average: " + statistics.average());
         System.out.println();
-        
-}
-                
+
+
+// part 3
+        System.out.println("Part 3");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter numbers:");
+
+        while (true) {
+            int number = scanner.nextInt();
+            if (number == -1) {
+                break; 
+            }
+            statistics.addNumber(number);
+        }
+
+        System.out.println("Sum of entered numbers: " + statistics.sum());
+        System.out.println();
+
+//part 4
+        System.out.println("part 4");
+
+        Statistics allNumbers = new Statistics();
+        Statistics evenNumbers = new Statistics();
+        Statistics oddNumbers = new Statistics();
+
+        System.out.println("Enter numbers:");
+
+
+        while (true) {
+            int number = scanner.nextInt();
+            if (number == -1) {
+                break; 
+            }
+            
+
+            allNumbers.addNumber(number);
+            
+
+            if (number % 2 == 0) {
+                evenNumbers.addNumber(number);
+            } else {
+                oddNumbers.addNumber(number);
+            }
+        }
+
+
+        System.out.println("Sum: " + allNumbers.sum());
+        System.out.println("Sum of even numbers: " + evenNumbers.sum());
+        System.out.println("Sum of odd numbers: " + oddNumbers.sum());
 }       
+}
