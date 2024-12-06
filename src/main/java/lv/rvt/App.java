467087459af5d9a3938d6a0ab35583ceb4132edd
23DP3TRube <166013447+23DP3TRube@users.noreply.github.com>
2024-12-06 08:46:22 +0000

    Health Station; Part 1;

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,21 +4,15 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        // example main program for the first section of the exercise
+    
+        HealthStation childrensHospital = new HealthStation();
+    
+        Person ethan = new Person("Ethan", 1, 110, 7);
+        Person peter = new Person("Peter", 33, 176, 85);
+    
+        System.out.println(ethan.getName() + " weight: " + childrensHospital.weigh(ethan) + " kilos");
+        System.out.println(peter.getName() + " weight: " + childrensHospital.weigh(peter) + " kilos");
 
-        Timer timer = new Timer();
-
-        while (true) {
-            System.out.println(timer);
-            timer.advance();
-
-            try {
-            Thread.sleep(10);
-    }       catch (Exception e) {
-
-    }
-        
 }
-}
-
 }
---
+++ src/main/java/lv/rvt/HealthStation.java
@@ -0,0 +1,13 @@
+package lv.rvt;
+
+public class HealthStation {
+
+    private int weight;
+
+    public int weigh(Person person) {
+        
+        return person.getWeight();
+         
+    }
+
+}
