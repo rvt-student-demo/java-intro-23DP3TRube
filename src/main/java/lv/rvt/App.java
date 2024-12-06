8384e99b2871775b1029d8905d97761696e7841b
23DP3TRube <166013447+23DP3TRube@users.noreply.github.com>
2024-12-06 09:21:09 +0000

    Card Payments; Part 4;

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,18 +5,21 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         PaymentTerminal unicafeExactum = new PaymentTerminal();
+        System.out.println(unicafeExactum);
 
-        double change = unicafeExactum.eatAffordably(10);
-        System.out.println("remaining change: " + change);
+        PaymentCard annesCard = new PaymentCard(2);
 
-        PaymentCard annesCard = new PaymentCard(7);
+        System.out.println("amount of money on the card is " + annesCard.balance() + " euros");
 
         boolean wasSuccessful = unicafeExactum.eatHeartily(annesCard);
         System.out.println("there was enough money: " + wasSuccessful);
+
+        unicafeExactum.addMoneyToCard(annesCard, 100);
+
         wasSuccessful = unicafeExactum.eatHeartily(annesCard);
         System.out.println("there was enough money: " + wasSuccessful);
-        wasSuccessful = unicafeExactum.eatAffordably(annesCard);
-        System.out.println("there was enough money: " + wasSuccessful);
+
+        System.out.println("amount of money on the card is " + annesCard.balance() + " euros");
 
         System.out.println(unicafeExactum);
     }
