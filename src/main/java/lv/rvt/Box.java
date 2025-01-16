84a8dcdeedd37e292cca0d77298d1d116e968f78
23DP3TRube <166013447+23DP3TRube@users.noreply.github.com>
2025-01-16 07:16:28 +0000

    Chapter 50 Programming Exercises part 1

---
+++ src/main/java/lv/rvt/Box.java
@@ -0,0 +1,35 @@
+package lv.rvt;
+
+public class Box {
+    private double width;
+    private double height;
+    private double length;
+
+
+    public Box( double width, double height, double length ){
+        this.height = height;
+        this.width = width;
+        this.length = length;
+    }
+
+
+    public Box(double side){
+    this(side, side, side);
+
+    }
+
+    // public Box(Box oldBox){
+    //     this.height = oldBox.height();
+    //     this.length = oldBox.length();
+    //     this.width = oldBox.width();
+    // }
+
+
+    public double volume(){
+        return this.height * this.length * this.width; 
+    }
+    public double area(){
+        return  2 * this.height + 2 * this.width + 2 * this.length ; 
+    }
+
+}
