bde6e2805d917dc543ba6de3549a09d7b6ccfeff
23DP3TRube <166013447+23DP3TRube@users.noreply.github.com>
2025-01-28 10:42:24 +0000

    Person and subclass Part 5

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,25 +1,35 @@
     package lv.rvt;
 
-    import java.util.Scanner;
+    import java.util.ArrayList;
+import java.util.Scanner;
 
 
     class App {
         public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-
+            // write your test code here
+    
             Teacher ada = new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200);
             Teacher esko = new Teacher("Esko Ukkonen", "Mannerheimintie 15 00100 Helsinki", 5400);
             System.out.println(ada);
             System.out.println(esko);
-
+    
             Student ollie = new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028");
-
+    
             int i = 0;
             while (i < 25) {
-            ollie.study();
-            i = i + 1;
+                ollie.study();
+                i = i + 1;
             }
             System.out.println(ollie);
-    }
-
+    
+        }
+    
+        public static void printPersons(ArrayList<Person> persons) {
+    
+            for (Person e : persons) {
+                System.out.println(e);
+            }
+    
+        }
+    
     }
