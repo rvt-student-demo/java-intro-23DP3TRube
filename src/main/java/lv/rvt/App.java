3b26ea6fe17f547e77fcbd9517f4766a7af6bc54
23DP3TRube <166013447+23DP3TRube@users.noreply.github.com>
2025-02-04 09:12:01 +0000

    Warehouse Part 5(salabots)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,6 +1,6 @@
-    package lv.rvt;
+package lv.rvt;
 
-    import java.util.ArrayList;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 
@@ -8,13 +8,13 @@ import java.util.Scanner;
         public static void main(String[] args) {
             
             ProductWarehouseWithHistory juice = new ProductWarehouseWithHistory("Juice", 1000.0, 1000.0);
-juice.takeFromWarehouse(11.3);
-System.out.println(juice.getName()); 
-juice.addToWarehouse(1.0);
-System.out.println(juice);          
+                juice.takeFromWarehouse(11.3);
+                System.out.println(juice.getName()); 
+                juice.addToWarehouse(1.0);
+                System.out.println(juice);          
 
 
-System.out.println(juice.history());
+                System.out.println(juice.history());
 
 }
 }
