3fc721536dbc0ae462ada51c1ac6f7bea9a744cb
23DP3TRube <166013447+23DP3TRube@users.noreply.github.com>
2025-02-04 08:23:55 +0000

    Warehouse Part 1

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,30 +6,13 @@ import java.util.Scanner;
 
     class App {
         public static void main(String[] args) {
-            // write your test code here
-    
-            Teacher ada = new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200);
-            Teacher esko = new Teacher("Esko Ukkonen", "Mannerheimintie 15 00100 Helsinki", 5400);
-            System.out.println(ada);
-            System.out.println(esko);
-    
-            Student ollie = new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028");
-    
-            int i = 0;
-            while (i < 25) {
-                ollie.study();
-                i = i + 1;
-            }
-            System.out.println(ollie);
-    
-        }
-    
-        public static void printPersons(ArrayList<Person> persons) {
-    
-            for (Person e : persons) {
-                System.out.println(e);
-            }
-    
-        }
+            
+            ProductWarehouse juice = new ProductWarehouse("Juice", 1000.0);
+                juice.addToWarehouse(1000.0);
+                juice.takeFromWarehouse(11.3);
+                System.out.println(juice.getName());
+                System.out.println(juice);
     
     }
+
+}
---
+++ src/main/java/lv/rvt/Warehouse.java
@@ -0,0 +1,51 @@
+package lv.rvt;
+
+public class Warehouse {
+
+    private double balance;
+    private double capacity;
+
+    public Warehouse(double capacity){
+        if (capacity > 0) {
+            this.capacity = capacity;
+        } else 
+        { this.capacity = 0;}
+           
+        this.balance = 0;
+    }
+    public double getBalance(){
+        return this.balance;
+    }
+    public double getCapacity(){
+        return this.capacity;
+    }
+    public double howMuchSpaceLeft(){
+        return this.capacity - this.balance;
+    }
+    public void addToWarehouse(double amount){
+     if (amount <= 0) {
+        return;
+     }   
+     if (amount <= this.howMuchSpaceLeft()){
+        this.balance = this.balance + amount;}
+            else {this.balance = this.capacity;}
+     }
+    
+
+    public double takeFromWarehouse(double amount){
+        if (amount <= 0) {
+            return 0.0; 
+        }
+
+        if (amount > this.balance) {
+            double allThatCanBeTaken = this.balance;
+            this.balance = 0.0; 
+            return allThatCanBeTaken;
+        }
+
+        this.balance = this.balance - amount;
+        return amount;
+        
+        
+    }
+}
