086da11341a4231d8b07ce3469fa47e42bc29a60
23DP3TRube <166013447+23DP3TRube@users.noreply.github.com>
2025-02-04 09:35:50 +0000

    Warehouse Part 7

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -9,12 +9,10 @@ import java.util.Scanner;
             
             ProductWarehouseWithHistory juice = new ProductWarehouseWithHistory("Juice", 1000.0, 1000.0);
                 juice.takeFromWarehouse(11.3);
-                System.out.println(juice.getName()); 
                 juice.addToWarehouse(1.0);
-                System.out.println(juice);          
 
 
-                System.out.println(juice.history());
+                juice.printAnalysis();
 
 }
 }
--- src/main/java/lv/rvt/ProductWarehouseWithHistory.java
+++ src/main/java/lv/rvt/ProductWarehouseWithHistory.java
@@ -31,4 +31,12 @@ public class ProductWarehouseWithHistory extends ProductWarehouse {
 
 
     }
+
+    public void printAnalysis(){
+        System.out.println("Product: " + getName());
+        System.out.println("History: " + history());
+        System.out.println("Largest amount of product: " + history.maxValue() );
+        System.out.println("Smallest amount of product: " + history.minValue());
+        System.out.println("Average: " + history.average());
+    }
 }
